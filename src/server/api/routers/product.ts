4b7893f3d8ae72e91112aad57de075d57dd9a8b3
Samuel <se12345a@email.me>
2023-12-31 23:48:21 +0700

    crud operations not all tested

--- src/server/api/routers/product.ts
+++ src/server/api/routers/product.ts
@@ -1,15 +1,66 @@
-import { productSchema } from "@/lib/schemas/product";
-import { createTRPCRouter, shopOwnerProcedure } from "@/server/api/trpc";
+import { ProductFields, productSchema } from "@/lib/schemas/product";
+import {
+  authenticatedProcedure,
+  createTRPCRouter,
+  shopOwnerProcedure,
+} from "@/server/api/trpc";
 import { db } from "@/server/db";
-import { products } from "@/server/db/schema";
+import { products, shops } from "@/server/db/schema";
+import { TRPCError } from "@trpc/server";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+
+const productIdInput = z.object({
+  productId: z.number(),
+});
+
+const ownProductProcedure = authenticatedProcedure
+  .input(productIdInput)
+  .use(async (opts) => {
+    const result = await db
+      .select({})
+      .from(products)
+      .innerJoin(shops, eq(shops.id, products.id));
+
+    if (!result.length) {
+      throw new TRPCError({ code: "NOT_FOUND" });
+    }
+
+    return opts.next();
+  });
 
 export const productRouter = createTRPCRouter({
   create: shopOwnerProcedure
     .input(productSchema)
     .mutation(async ({ ctx, input }) => {
-      await db.insert(products).values({
-        ...input,
-        shopId: ctx.shopId,
-      });
+      await db
+        .insert(products)
+        .values({
+          ...input,
+          shopId: ctx.shopId,
+        })
+        .onDuplicateKeyUpdate({
+          set: input,
+        });
     }),
+
+  update: ownProductProcedure
+    .input(productSchema)
+    .mutation(async ({ input }) => {
+      await db
+        .update(products)
+        .set(input)
+        .where(eq(products.id, input.productId));
+    }),
+
+  delete: ownProductProcedure.mutation(async ({ input }) => {
+    await db.delete(products).where(eq(products.id, input.productId));
+  }),
+
+  myProducts: shopOwnerProcedure.query(async ({ ctx }) => {
+    return await db
+      .select()
+      .from(products)
+      .where(eq(products.shopId, ctx.shopId));
+  }),
 });
