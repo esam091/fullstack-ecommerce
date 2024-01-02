97007ded2f8ebe9eca6ea0d04f51e427dfd4c695
Samuel <se12345a@email.me>
2024-01-02 15:05:19 +0700

    product empty view

--- src/app/my-shop/(sidebar)/products/page.tsx
+++ src/app/my-shop/(sidebar)/products/page.tsx
@@ -1,8 +1,25 @@
 import { api } from "@/trpc/server";
 import ProductRow from "./product-row";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Page() {
   const products = await api.product.myProducts.query();
+
+  if (products.length === 0) {
+    return (
+      <div>
+        <h2>No products found</h2>
+
+        <p>Add your first product and start selling</p>
+
+        <Button asChild>
+          <Link href={"/my-shop/products/add"}>Add Product</Link>
+        </Button>
+      </div>
+    );
+  }
+
   return (
     <div>
       {products.map((product) => (
