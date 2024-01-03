eb75ae267d69eda343d11f36fde92285d7d8585b
Samuel <se12345a@email.me>
2024-01-03 16:28:07 +0700

    usecombobox

--- src/app/my-shop/(sidebar)/catalog/add-edit-form.tsx
+++ src/app/my-shop/(sidebar)/catalog/add-edit-form.tsx
@@ -11,7 +11,7 @@ import imageUrl from "@/lib/imageUrl";
 import { type products } from "@/server/db/schema";
 import { PopoverAnchor } from "@radix-ui/react-popover";
 import clsx from "clsx";
-import Downshift from "downshift";
+import Downshift, { useCombobox } from "downshift";
 import Image from "next/image";
 import { useId } from "react";
 
@@ -36,69 +36,69 @@ type ProductAutocompleteProps = {
 };
 
 function ProductAutocomplete({ products }: ProductAutocompleteProps) {
+  const {
+    getLabelProps,
+    getInputProps,
+    isOpen,
+    getMenuProps,
+    getItemProps,
+
+    inputValue,
+    highlightedIndex,
+    reset,
+  } = useCombobox({
+    items: products,
+    onSelectedItemChange(changes) {
+      alert("change");
+      reset();
+    },
+  });
+
   return (
-    <Downshift<Product>
-      onChange={() => alert("clicked")}
-      itemToString={(product) => product?.name ?? ""}
-    >
-      {({
-        getLabelProps,
-        getInputProps,
-        isOpen,
-        getMenuProps,
-        getItemProps,
-        getRootProps,
-        inputValue,
-        highlightedIndex,
-      }) => (
-        <div {...getRootProps({}, { suppressRefError: true })}>
-          <Label {...getLabelProps()}>Select products</Label>
-          <Input {...getInputProps()} />
+    <div>
+      <Label {...getLabelProps()}>Select products</Label>
+      <Input {...getInputProps()} />
 
-          <Popover open={isOpen} {...getMenuProps()}>
-            <PopoverAnchor />
-            <PopoverContent
-              className="p-0"
-              align="start"
-              onOpenAutoFocus={(e) => e.preventDefault()}
-            >
-              <ul {...getMenuProps({}, { suppressRefError: true })}>
-                {products
-                  .filter(
-                    (product) =>
-                      !!inputValue &&
-                      product.name
-                        .toLowerCase()
-                        .includes(inputValue.toLowerCase()),
-                  )
-                  .map((product, index) => (
-                    <li
-                      key={product.id}
-                      {...getItemProps({ item: product, index })}
-                      className={clsx(
-                        "flex cursor-pointer items-center p-4",
-                        highlightedIndex === index && "bg-muted",
-                      )}
-                    >
-                      <Image
-                        alt={`Image of ${product.name}`}
-                        className="mr-3 rounded-md"
-                        width={50}
-                        height={50}
-                        src={imageUrl(product.image)}
-                      />
+      <Popover open={isOpen} {...getMenuProps()}>
+        <PopoverAnchor />
+        <PopoverContent
+          className="p-0"
+          align="start"
+          onOpenAutoFocus={(e) => e.preventDefault()}
+        >
+          <ul {...getMenuProps({}, { suppressRefError: true })}>
+            {products
+              .filter(
+                (product) =>
+                  !!inputValue &&
+                  product.name.toLowerCase().includes(inputValue.toLowerCase()),
+              )
+              .map((product, index) => (
+                <li
+                  key={product.id}
+                  {...getItemProps({ item: product, index })}
+                  className={clsx(
+                    "flex cursor-pointer items-center p-4",
+                    highlightedIndex === index && "bg-muted",
+                  )}
+                >
+                  <Image
+                    alt={`Image of ${product.name}`}
+                    className="mr-3 rounded-md"
+                    width={50}
+                    height={50}
+                    src={imageUrl(product.image)}
+                  />
 
-                      <div>
-                        <div className="font-semibold">{product.name}</div>
-                        <div className="text-sm">$ {product.price}</div>
-                      </div>
-                    </li>
-                  ))}
-              </ul>
-            </PopoverContent>
-          </Popover>
-        </div>
-      )}
-    </Downshift>
+                  <div>
+                    <div className="font-semibold">{product.name}</div>
+                    <div className="text-sm">$ {product.price}</div>
+                  </div>
+                </li>
+              ))}
+          </ul>
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 }
