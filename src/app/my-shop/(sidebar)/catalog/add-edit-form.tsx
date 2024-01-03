8055f1a0e73a5efef0f79b2a16c30b962ad3f4e5
Samuel <se12345a@email.me>
2024-01-03 15:08:41 +0700

    trying to build autocomplete

--- src/app/my-shop/(sidebar)/catalog/add-edit-form.tsx
+++ src/app/my-shop/(sidebar)/catalog/add-edit-form.tsx
@@ -1,4 +1,61 @@
 "use client";
-export default function CatalogForm() {
-  return <div>catalog form</div>;
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { type products } from "@/server/db/schema";
+import Downshift from "downshift";
+import { useId } from "react";
+
+type Props = {
+  products: Array<typeof products.$inferSelect>;
+};
+
+export default function CatalogForm({ products }: Props) {
+  return (
+    <div>
+      <div>catalog form</div>
+
+      <ProductAutocomplete products={products} />
+    </div>
+  );
+}
+
+type Product = typeof products.$inferSelect;
+
+type ProductAutocompleteProps = {
+  products: Product[];
+};
+
+function ProductAutocomplete({ products }: ProductAutocompleteProps) {
+  return (
+    <Downshift<Product>
+      onChange={() => alert("clicked")}
+      itemToString={(product) => product?.name ?? ""}
+    >
+      {({
+        getLabelProps,
+        getInputProps,
+        isOpen,
+        getMenuProps,
+        getItemProps,
+      }) => (
+        <div>
+          <Label {...getLabelProps()}>Select products</Label>
+          <Input {...getInputProps()} />
+
+          <ul {...getMenuProps()}>
+            {isOpen &&
+              products.map((product, index) => (
+                <li
+                  key={product.id}
+                  {...getItemProps({ item: product, index })}
+                >
+                  {product.id} {product.name}
+                </li>
+              ))}
+          </ul>
+        </div>
+      )}
+    </Downshift>
+  );
 }
