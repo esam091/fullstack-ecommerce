503fe514d0f46acecef8ff4cd9767883ce030582
Samuel <se12345a@email.me>
2024-01-05 16:00:13 +0700

    turn whole card into link

--- src/app/my-shop/(sidebar)/catalog/page.tsx
+++ src/app/my-shop/(sidebar)/catalog/page.tsx
@@ -15,31 +15,29 @@ export default async function Page() {
   return (
     <div className="space-y-8">
       <h3 className="text-xl font-semibold">My Catalogs</h3>
-      <div className="space-y-6">
+      <div className="flex flex-col gap-6">
         {myCatalogs.map((item) => (
-          <Card key={item.catalog.id}>
-            <CardHeader>
-              <CardTitle>
-                <Link href={`/my-shop/catalog/edit/${item.catalog.id}`}>
-                  {item.catalog.name}
-                </Link>
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="grid grid-cols-2 gap-4 ">
-              {item.products.map((product) => (
-                <div key={product.id} className="flex items-center">
-                  <Image
-                    src={imageUrl(product.image)}
-                    width={40}
-                    height={40}
-                    className="mr-3 h-10 w-10 rounded-md object-cover"
-                    alt={`Image of ${product.name}`}
-                  />{" "}
-                  <span className="text-sm">{product.name}</span>
-                </div>
-              ))}
-            </CardContent>
-          </Card>
+          <Link href={`/my-shop/catalog/edit/${item.catalog.id}`}>
+            <Card key={item.catalog.id}>
+              <CardHeader>
+                <CardTitle>{item.catalog.name}</CardTitle>
+              </CardHeader>
+              <CardContent className="grid grid-cols-2 gap-4 ">
+                {item.products.map((product) => (
+                  <div key={product.id} className="flex items-center">
+                    <Image
+                      src={imageUrl(product.image)}
+                      width={40}
+                      height={40}
+                      className="mr-3 h-10 w-10 rounded-md object-cover"
+                      alt={`Image of ${product.name}`}
+                    />{" "}
+                    <span className="text-sm">{product.name}</span>
+                  </div>
+                ))}
+              </CardContent>
+            </Card>
+          </Link>
         ))}
       </div>
     </div>
