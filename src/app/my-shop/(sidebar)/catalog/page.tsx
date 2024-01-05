01ffe175637bf4b80d53de4839d1d512c9356b54
Samuel <se12345a@email.me>
2024-01-05 15:55:10 +0700

    catalog card

--- src/app/my-shop/(sidebar)/catalog/page.tsx
+++ src/app/my-shop/(sidebar)/catalog/page.tsx
@@ -1,3 +1,42 @@
-export default function Page() {
-  return <div>my catalogs</div>;
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import imageUrl from "@/lib/imageUrl";
+import { api } from "@/trpc/server";
+import Image from "next/image";
+
+export default async function Page() {
+  const myCatalogs = await api.catalog.myCatalogs.query();
+  return (
+    <div className="space-y-8">
+      <h3 className="text-xl font-semibold">My Catalogs</h3>
+      <div className="space-y-6">
+        {myCatalogs.map((item) => (
+          <Card key={item.catalog.id}>
+            <CardHeader>
+              <CardTitle>{item.catalog.name}</CardTitle>
+            </CardHeader>
+            <CardContent className="grid grid-cols-2 gap-4 ">
+              {item.products.map((product) => (
+                <div key={product.id} className="flex items-center">
+                  <Image
+                    src={imageUrl(product.image)}
+                    width={40}
+                    height={40}
+                    className="mr-3 h-10 w-10 rounded-md object-cover"
+                    alt={`Image of ${product.name}`}
+                  />{" "}
+                  <span className="text-sm">{product.name}</span>
+                </div>
+              ))}
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 }
