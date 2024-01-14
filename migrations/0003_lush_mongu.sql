e6e3c63bac13803d6d0c45d15bd95b9ff3ab79e4
Samuel <se12345a@email.me>
2024-01-14 20:20:09 +0700

    create category

---
+++ migrations/0003_lush_mongu.sql
@@ -0,0 +1,9 @@
+-- Custom SQL migration file, put you code below! --
+INSERT INTO `ep_category`(id, name) VALUES
+('uXBw8xaXCU_IusYdUlMOQ', 'Electronics and Gadgets'),
+('xqxwlVdQ1K7poGyHz4axx', 'Home and Kitchen Appliances'),
+('NgFLvkxqrR8IeVPGkqtc3', 'Handmade and Craft Items'),
+('9cUuoKPGFcds41O0N1TZ9', 'Office Supplies'),
+('KNhlz4I76tYHWM4XikH8P', 'Gardening Tools and Plants'),
+('Y1s9mYD1jpeUfVxUgttTO', 'DIY and Hardware Tools')
+;
