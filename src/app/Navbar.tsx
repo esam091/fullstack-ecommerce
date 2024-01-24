af51b3e58b22f8e3681dce436e99dfe321807656
Samuel <se12345a@email.me>
2024-01-24 09:23:59 +0700

    navbar container

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,20 +1,25 @@
 import { ShoppingBag } from "lucide-react";
 import { api } from "@/trpc/server";
 import UserSection from "./user-section";
+import Link from "next/link";
 
 export default async function NavBar() {
   const shop = await api.shop.myShop.query();
 
   return (
-    <header className="flex h-16 items-center self-stretch border-b border-b-border bg-background px-6">
-      <span className="flex items-center gap-1 text-primary">
-        <ShoppingBag size={32} />
-        <span className="text-xl">Happy Commerce</span>
-      </span>
+    <header className="h-16 w-full items-center border-b border-b-border bg-background">
+      <div className="container mx-auto flex h-full items-center">
+        <Link href={"/"}>
+          <span className="flex items-center gap-1 text-primary">
+            <ShoppingBag size={32} />
+            <span className="text-xl">Happy Commerce</span>
+          </span>
+        </Link>
 
-      <nav className="ml-auto flex gap-5">
-        <UserSection shop={shop} />
-      </nav>
+        <nav className="ml-auto flex gap-5">
+          <UserSection shop={shop} />
+        </nav>
+      </div>
     </header>
   );
 }
